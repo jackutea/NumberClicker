3763df7c6f6915b7c550fb0729724ec7c97b3f73
杰克有茶 <chenwansal1@163.com>
2023-04-12 22:47:56 +0800

    ESTABLISH

---
+++ Assets/Scripts_Runtime/Main.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace NumberClicker {
+
+    public class Main : MonoBehaviour {
+
+        void Awake() {
+            Debug.Log("Awake");
+        }
+
+    }
+
+}
